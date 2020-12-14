a4bde28bf8322b1f9e5c3202f40e80bbe305c543
Ahmet Gürdal <ahmetgurdal32@gmail.com>
2020-12-14 22:05:40 +0300

    E: Eslint

---
+++ src/production-efficiency/models/index.tsx
@@ -0,0 +1,26 @@
+
+/*
+
+{
+	"Requirements":{
+		"Minerals":
+		{
+			"Nitrogen":
+			{
+				"Unit" : "%",
+				"Min"  : 2.28,
+				"Max"  : 4.89,
+				"Average" : 3.9
+			},
+        }
+    }
+}
+
+*/
+
+export type MineralRequirements ={
+    unit: string
+    min: number
+    max: number
+    average: number
+}
