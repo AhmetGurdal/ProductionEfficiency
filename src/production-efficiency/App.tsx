6e16f5260e4497c1e95736d0208a6dde71ea2111
Ahmet Gürdal <ahmetgurdal32@gmail.com>
2020-12-14 23:31:44 +0300

    I: Fixed

--- src/production-efficiency/App.tsx
+++ src/production-efficiency/App.tsx
@@ -1,20 +1,14 @@
-// ---------------------------
-//  External Dependencies
-// ---------------------------
+// eslint-disable-next-line no-use-before-define
 import React from 'react'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 
-// ---------------------------
-//  Project Dependencies
-// ---------------------------
 import { NavigBar } from 'production-efficiency/components'
 import { About, Home } from 'production-efficiency/pages'
 
 const App = () => (
     <Router>
         <div>
-            <NavigBa />
-r
+            <NavigBar />
             <main className='pt-4'>
                 <Switch>
                     <Route path='/about'>
--- src/production-efficiency/pages/Home.tsx
+++ src/production-efficiency/pages/Home.tsx
@@ -1,3 +1,4 @@
+// eslint-disable-next-line no-use-before-define
 import React from 'react'
 import { Container, Row, Col, Button, Form } from 'react-bootstrap'
 
